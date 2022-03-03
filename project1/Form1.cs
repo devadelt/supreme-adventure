4c0d8bf82dcd0579901ef493d1d856e59df6ad7f
devadelt <devaraja@student.telkomuniversity.ac.id>
2022-03-04 00:42:43 +0700

    tambahan fitur GUI

--- project1/Form1.cs
+++ project1/Form1.cs
@@ -16,5 +16,12 @@ namespace project1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string d = textBox1.Text;
+
+            label1.Text = "Halo " + d;
+        }
     }
 }
